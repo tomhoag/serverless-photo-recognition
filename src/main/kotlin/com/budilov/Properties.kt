1ee178e675ed786357e23c18e3f3ad063c8b41b9
Tom <tomhoag@Gmail.com>
2017-03-01 07:29:14 -0500

    not sure . . .

--- src/main/kotlin/com/budilov/Properties.kt
+++ src/main/kotlin/com/budilov/Properties.kt
@@ -8,15 +8,15 @@ package com.budilov
  */
 
 object Properties {
-    val _REGION = "REGION_REPLACE_ME"
-    val _ACCOUNT_NUMBER = "ACCOUNT_REPLACE_ME"
-    val _BUCKET_NAME = "BUCKET_REPLACE_ME"
+    val _REGION = "us-east-1"
+    val _ACCOUNT_NUMBER = "266942040790"
+    val _BUCKET_NAME = "rekognition-20170228123539"
     val _BUCKET_URL = "." + _BUCKET_NAME + "-" + _REGION + ".amazonaws.com"
-    val _ES_SERVICE_URL = "https://ES_SERVICE_URL_REPLACE_ME"
+    val _ES_SERVICE_URL = "https://"
     val _REKOGNITION_URL = "https://rekognition." + _REGION + ".amazonaws.com"
     val _REKOGNITION_CONFIDENCE_THRESHOLD = 60
-    val _COGNITO_POOL_ID = "COGNITO_POOL_ID_REPLACE_ME"
-    val _USER_POOL_ID = "USER_POOL_ID_REPLACE_ME"
+    val _COGNITO_POOL_ID = "us-east-1:e74be541-677b-4ace-8054-0a934d9c549d"
+    val _USER_POOL_ID = "us-east-1_OeAshXqhs"
     val _COGNITO_POOL_ID_IDP_NAME = "cognito-idp." + _REGION + ".amazonaws.com/" + _USER_POOL_ID
     val _S3_SIGNED_URL_DURATION = 1
 }
